8e2b7442a95be75a5a2cad57bf1d34b5da139961
Aaron <aaronpromail@gmail.com>
2024-05-23 00:49:45 +0100

    feature : generate meal recipe - retrieval of submitted form fields

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request, Response
+from typing import Optional, Annotated
+from fastapi import FastAPI, Request, Response, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -10,4 +11,17 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 async def home(request: Request) -> Response:
     """Home page."""
+    return templates.TemplateResponse("home.jinja", {"request": request})
+
+
+@app.post("/")
+async def generate_meal_recipe(
+    request: Request,
+    query: Annotated[str, Form()],
+    cuisine: Annotated[Optional[str], Form()] = None,
+    type: Annotated[Optional[str], Form()] = None,
+    diet: Annotated[Optional[str], Form()] = None,
+    intolerances: Annotated[Optional[str], Form()] = None,
+) -> Response:
+    """Retrieve generated meal recipe."""
     return templates.TemplateResponse("home.jinja", {"request": request})
